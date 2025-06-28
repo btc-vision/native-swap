3baa47989c3fffa41c7396c5a2e3305ac9786aea
Anakun <96896824+BlobMaster41@users.noreply.github.com>
2025-06-28 22:30:33 +0400

    Update Contract.ts

--- src/constants/Contract.ts
+++ src/constants/Contract.ts
@@ -1,13 +1,14 @@
 import { u128, u256 } from '@btc-vision/as-bignum/assembly';
 
 export const FEE_COLLECT_SCRIPT_PUBKEY: string =
-    'bcrt1plz0svv3wl05qrrv0dx8hvh5mgqc7jf3mhqgtw8jnj3l3d3cs6lzsfc3mxh';
+    'tb1p823gdnqvk8a90f8cu30w8ywvk29uh8txtqqnsmk6f5ktd7hlyl0q3cyz4c';
+//'bcrt1plz0svv3wl05qrrv0dx8hvh5mgqc7jf3mhqgtw8jnj3l3d3cs6lzsfc3mxh';
 
 // tb1p823gdnqvk8a90f8cu30w8ywvk29uh8txtqqnsmk6f5ktd7hlyl0q3cyz4c
 
 export const QUOTE_SCALE: u256 = u256.fromU64(100_000_000);
-export const RESERVATION_EXPIRE_AFTER_IN_BLOCKS: u64 = 5;
-export const VOLATILITY_WINDOW_IN_BLOCKS: u32 = 5;
+export const RESERVATION_EXPIRE_AFTER_IN_BLOCKS: u64 = 9;
+export const VOLATILITY_WINDOW_IN_BLOCKS: u32 = 9;
 export const STRICT_MINIMUM_PROVIDER_RESERVATION_AMOUNT_IN_SAT: u64 = 600;
 export const MINIMUM_PROVIDER_RESERVATION_AMOUNT_IN_SAT: u64 = 1000;
 export const MINIMUM_LIQUIDITY_VALUE_ADD_LIQUIDITY_IN_SAT: u64 = 10_000;
@@ -25,7 +26,7 @@ export const INITIAL_LIQUIDITY_PROVIDER_INDEX: u32 = u32.MAX_VALUE - 1;
 export const MAXIMUM_VALID_INDEX: u32 = u32.MAX_VALUE - 2;
 export const BLOCK_NOT_SET_VALUE: u64 = U64.MAX_VALUE;
 
-export const EMIT_PURGE_EVENTS: boolean = true;
+export const EMIT_PURGE_EVENTS: boolean = false;
 
 export const ALLOW_DIRTY: boolean = true;
 
