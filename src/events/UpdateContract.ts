a9060b587722a83a8d297625af1162bf7d06bb1b
matbout <48333440+matbout@users.noreply.github.com>
2026-02-27 14:15:25 -0500

    Rename upgrade to update

--- src/events/UpdateContract.ts
+++ src/events/UpdateContract.ts
@@ -6,11 +6,11 @@ import {
 } from '@btc-vision/btc-runtime/runtime';
 
 @final
-export class UpgradeContract extends NetEvent {
+export class UpdateContract extends NetEvent {
     constructor(address: Address) {
         const data: BytesWriter = new BytesWriter(ADDRESS_BYTE_LENGTH);
         data.writeAddress(address);
 
-        super('Upgraded', data);
+        super('Updated', data);
     }
 }
